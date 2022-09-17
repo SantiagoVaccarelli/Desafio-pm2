bb8c6a9b9af433cd6064935ed09d60c94edfd43c
Santiago Vaccarelli <svaccarelli@fi.uba.ar>
2022-09-17 13:56:34 -0300

    Log and profiling done

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { fork } from "child_process";
+import compression from "compression";
+import logger from "../utils/logger.js";
 import os from "os";
 const nroCPUs = os.cpus().length;
 const router = Router();
@@ -17,7 +19,6 @@ const info = {
 };
 
 router.get("/", (req, res) => {
-  res.send(info);
   res.redirect("/info");
 });
 
@@ -25,18 +26,36 @@ router.get("/info", (req, res) => {
   res.send(info);
 });
 
+router.get("/info-gzip", compression(), (req, res) => {
+  logger.info("Route: /info-gzip Method: GET ");
+  res.send(info);
+})
+
 router.get("/api/randoms", (req, res) => {
   //http://localhost:3000/api/randoms?cant=1000
-  const cant = req.query.cant || 100000000;
-  const child = fork("./src/getRandom.js");
-  child.send(cant);
-  child.on("message", (msg) => {
-    res.send(msg);
-  });
+  
+  if (!req.query.cant) {
+    logger.error(
+      "Route: /api/randoms Method: GET Error: cantidad no especificada"
+    );
+    res.status(400).send("Debe indicar la cantidad de números a generar");
+  }
+  else{
+    const cant = req.query.cant;
+    const child = fork("./src/utils/getRandoms.js");
+    child.send(cant);
+    child.on("message", (msg) => {
+      res.send(msg);
+    })
+    child.on("exit", (code) => {
+      console.log("Se ha cerrado el proceso", code);
+    })
+  }
+});
 
-  child.on("exit", (code) => {
-    console.log("Se ha cerrado el proceso", code);
-  });
+router.get("*", compression(), (req, res) => {
+  logger.warn("Route: 404 Not Found Method: GET ");
+  res.send("Sorry 404 Not Found");
 });
 
 export default router;
